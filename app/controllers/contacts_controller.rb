f9b48ec008af1e78d7e63a2a33f2992070abcc9a
Chris Woodside <christopherwoodside@gmail.com>
2016-11-14 19:31:09 -0600

    Initial commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,35 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'destroy.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    render 'create.html.erb'
+  end
+
+  def show
+    render 'show.html.erb'
+  end
+
+  def edit
+    render 'edit.html.erb'
+  end
+
+  def update
+    render 'update.html.erb'
+  end
+
+  def destroy
+    render 'destroy.html.erb'
+  end
+
+
+
+
+
+end
