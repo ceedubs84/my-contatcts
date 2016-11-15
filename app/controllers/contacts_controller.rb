e2f1a8cc41d3b21a49a5aee40bcba619be535d6b
Chris Woodside <christopherwoodside@gmail.com>
2016-11-15 11:18:44 -0600

    Added controllers and update views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
-    render 'destroy.html.erb'
+    render 'index.html.erb'
   end
 
   def new
@@ -9,22 +9,42 @@ class ContactsController < ApplicationController
   end
 
   def create
-    render 'create.html.erb'
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number]
+      )
+    contact.save
+    flash[:success] = "Contact Created Successfully!!"
+    redirect_to "/contacts"
   end
 
   def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
     render 'show.html.erb'
   end
 
   def edit
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
     render 'edit.html.erb'
   end
 
   def update
-    render 'update.html.erb'
+    contact_id = params[:id]
+    contact = Contact.find_by(id: contact_id)
+    contact.first_name = params[:first_name]
+    contact.last_name = params[:last_name]
+    contact.phone_number = params[:phone_number]
+    contact.save
+    redirect_to '/contacts/#{contact.id}'
   end
 
   def destroy
+    contact_id = params[:id]
+    contact = Contact.find_by(id: contact_id)
+    contact.destroy
     render 'destroy.html.erb'
   end
 
